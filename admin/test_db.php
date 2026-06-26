28edc7e3cc1b234823c7dfe6d51ff3e223ced2c1
digitalengineer07 <nikhil119124@gmail.com>
2026-06-26 21:14:52 +0530

    feat: add test_db.php to verify payment_reminders table schema

--- admin/test_db.php
+++ admin/test_db.php
@@ -1,5 +1,5 @@
 <?php
-require 'db.php';
+require '../db.php';
 $res = mysqli_query($conn, "DESCRIBE payment_reminders");
 while($row = mysqli_fetch_assoc($res)) {
     echo $row['Field'] . ' - ' . $row['Type'] . PHP_EOL;
