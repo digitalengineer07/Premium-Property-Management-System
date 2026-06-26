663ffe02cda7091ddaafdbf21d56e76df7575e23
digitalengineer07 <nikhil119124@gmail.com>
2026-06-26 22:12:38 +0530

    feat: add test script to verify database connectivity and retrieve rent months

--- admin/test_db.php
+++ admin/test_db.php
@@ -1,7 +1,4 @@
 <?php
 require '../db.php';
-$r = mysqli_query($conn, "SHOW CREATE TABLE rent");
-echo mysqli_fetch_assoc($r)['Create Table']."\n\n";
-
-$r = mysqli_query($conn, "SHOW CREATE TABLE electricity");
-echo mysqli_fetch_assoc($r)['Create Table']."\n\n";
+$r = mysqli_query($conn, "SELECT DISTINCT month FROM rent");
+while($row=mysqli_fetch_assoc($r)) echo $row['month'].', ';
