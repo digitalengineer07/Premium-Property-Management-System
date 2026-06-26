db95c1af2ff60fe0cf70af0658f59ebe6793d509
digitalengineer07 <nikhil119124@gmail.com>
2026-06-26 21:56:50 +0530

    feat: add test script to query user payment statistics from database

--- admin/test_db.php
+++ admin/test_db.php
@@ -1,6 +1,14 @@
 <?php
 require '../db.php';
-$res = mysqli_query($conn, "DESCRIBE electricity");
-while($r = mysqli_fetch_assoc($res)){
-    echo $r['Field']."\n";
-}
+$q = "
+    SELECT u.id, u.name, u.room_no, u.photo,
+           IFNULL((SELECT SUM(rent_amount + maintenance + extra_charges + total_amount) FROM electricity WHERE user_id = u.id AND status='Paid'), 0) +
+           IFNULL((SELECT SUM(rent_amount) FROM rent WHERE user_id = u.id AND status='Paid'), 0) as total_paid,
+           IFNULL((SELECT SUM(total_amount) FROM electricity WHERE user_id = u.id AND status='Due'), 0) +
+           IFNULL((SELECT SUM(rent_amount) FROM rent WHERE user_id = u.id AND status='Due'), 0) as total_due
+    FROM users u
+    WHERE u.status='active'
+    ORDER BY total_paid DESC LIMIT 4
+";
+$res = mysqli_query($conn, $q);
+if(!$res) echo mysqli_error($conn);
