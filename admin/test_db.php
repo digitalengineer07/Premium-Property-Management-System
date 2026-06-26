117ce6e34b3404e08cef70b915408229c9098c8f
digitalengineer07 <nikhil119124@gmail.com>
2026-06-26 21:14:41 +0530

    feat: add database schema diagnostic script for payment_reminders table

--- admin/test_db.php
+++ admin/test_db.php
@@ -1,12 +1,6 @@
 <?php
-require '../db.php';
-echo "--- USERS ---" . PHP_EOL;
-$res = mysqli_query($conn, "SELECT * FROM users");
+require 'db.php';
+$res = mysqli_query($conn, "DESCRIBE payment_reminders");
 while($row = mysqli_fetch_assoc($res)) {
-    echo $row['id'] . " - " . $row['name'] . " - " . ($row['status'] ?? 'no status col') . PHP_EOL;
-}
-echo "--- ELECTRICITY ---" . PHP_EOL;
-$res = mysqli_query($conn, "SELECT * FROM electricity");
-while($row = mysqli_fetch_assoc($res)) {
-    echo $row['id'] . " - uid: " . $row['user_id'] . " - " . $row['status'] . PHP_EOL;
+    echo $row['Field'] . ' - ' . $row['Type'] . PHP_EOL;
 }
