ac7930504483063482d518fa7d402af64fc6e64f
digitalengineer07 <nikhil119124@gmail.com>
2026-06-27 11:04:15 +0530

    fix: update database connection path in schema.php

--- schema.php
+++ schema.php
@@ -1,5 +1,5 @@
 <?php
-require 'includes/db.php';
+require 'db.php';
 $res = mysqli_query($conn, "DESCRIBE electricity");
 while($r = mysqli_fetch_assoc($res)) {
     echo $r['Field'] . ' ' . $r['Type'] . "\n";
