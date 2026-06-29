984e953c14db323142ea6abdd72569d2b8c5996b
digitalengineer07 <nikhil119124@gmail.com>
2026-06-30 02:01:28 +0530

    chore: remove scratch_test_fn.php development utility script

--- scratch_test_fn.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "db.php";
-$_SESSION['user_id'] = 2; // rinku202
-require "renter/fetch_notifications.php";
-echo "Unread count: $unread_count\n";
-unlink('scratch_test_fn.php');
-?>
