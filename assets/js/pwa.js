dae1430ab6eeb662539c7fe162c502e314c8d17f
digitalengineer07 <nikhil119124@gmail.com>
2026-07-03 21:10:07 +0530

    feat: add PWA installation prompt and floating install button UI

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -2,6 +2,7 @@ let deferredPrompt;
 
 // Create the floating install button
 const installBtn = document.createElement('button');
+installBtn.id = 'pwaInstallBtn';
 installBtn.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="margin-right: -2px;">
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
