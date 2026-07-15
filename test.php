850cd73a2e72974326c3a4e7ff5e485d7fa6dfde
digitalengineer07 <nikhil119124@gmail.com>
2026-07-16 00:12:27 +0530

    chore: remove unused test script for database connectivity validation

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-$res = mysqli_query($conn, "SELECT COUNT(*) as count FROM users WHERE status = 'active'");
-if (!$res) {
-    echo "SQL ERROR: " . mysqli_error($conn) . "\n";
-} else {
-    echo "Query succeeded.\n";
-}
