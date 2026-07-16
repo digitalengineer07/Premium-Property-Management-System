15a76aadd29caabaefd2e8d5d559d5754e2c1eaf
digitalengineer07 <nikhil119124@gmail.com>
2026-07-16 10:01:20 +0530

    feat: implement mobile-responsive profile view for user account management

--- renter/views/mobile/profile_mobile.php
+++ renter/views/mobile/profile_mobile.php
@@ -272,6 +272,7 @@ $linked_docs_count = (!empty($user['aadhaar_file']) ? 1 : 0) + (!empty($user['ag
         font-size: 15px;
         font-weight: 700;
         text-decoration: none;
+        margin-top: auto;
         margin-bottom: 32px;
     }
 
