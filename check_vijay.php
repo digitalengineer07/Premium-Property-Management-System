c0b00f83e9ef429fcf72d47673e2fda95afa9a93
digitalengineer07 <nikhil119124@gmail.com>
2026-07-20 11:36:10 +0530

    refactor: simplify user payment query to fetch advance_payment for specific ID

--- check_vijay.php
+++ check_vijay.php
@@ -1,6 +1,4 @@
 <?php
 require 'db.php';
-$q = mysqli_query($conn, "SELECT * FROM payments WHERE user_id = (SELECT id FROM users WHERE name LIKE '%Test User%' LIMIT 1)");
-while($r = mysqli_fetch_assoc($q)) {
-    print_r($r);
-}
+$r = mysqli_fetch_assoc(mysqli_query($conn, "SELECT advance_payment FROM users WHERE id = 8"));
+print_r($r);
