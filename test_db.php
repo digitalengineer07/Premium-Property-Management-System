fa693bf0eb3402c492bc84e9f0d62b733099d24f
digitalengineer07 <nikhil119124@gmail.com>
2026-07-24 22:39:10 +0530

    refactor: update test_db.php to query and display user advance and security deposit information

--- test_db.php
+++ test_db.php
@@ -1,9 +1,7 @@
 <?php
 require 'db.php';
-$res = mysqli_query($conn, 'SELECT * FROM payments LIMIT 5');
-while($r = mysqli_fetch_assoc($res)) print_r($r);
-echo "\n====\n";
-$res2 = mysqli_query($conn, 'SELECT * FROM electricity LIMIT 5');
-while($r = mysqli_fetch_assoc($res2)) {
-    echo "ID: {$r['id']}, Rent: {$r['rent_amount']}, Elec: {$r['amount']}, Total: {$r['total_amount']}, Status: {$r['status']}\n";
+// Check current advance and security deposit
+$res = mysqli_query($conn, 'SELECT id, name, advance_payment, security_deposit FROM users');
+while($r = mysqli_fetch_assoc($res)) {
+    print_r($r);
 }
