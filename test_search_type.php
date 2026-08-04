a6ce22a08520555dd9f2d41a177e8a4b48c52833
digitalengineer07 <nikhil119124@gmail.com>
2026-08-05 01:53:31 +0530

    feat: add script to extract type assignment lines from my-bills.php for debugging

---
+++ test_search_type.php
@@ -0,0 +1,8 @@
+<?php
+$lines = file('c:\xampp\htdocs\renter-system\renter\my-bills.php');
+foreach ($lines as $i => $line) {
+    if (strpos($line, '$type =') !== false) {
+        echo ($i+1) . ": " . trim($line) . "\n";
+    }
+}
+?>
