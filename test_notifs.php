c7bb808732b448fb66bd147bf23935de6228d856
digitalengineer07 <nikhil119124@gmail.com>
2026-08-05 02:23:08 +0530

    chore: remove obsolete notification testing script

--- test_notifs.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db.php';
-$res = mysqli_query($conn, "SELECT id, bill_type, bill_id, month, amount FROM payment_notifications");
-while($row = mysqli_fetch_assoc($res)) {
-    print_r($row);
-}
-?>
