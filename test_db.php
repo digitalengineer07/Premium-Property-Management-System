97810774a4ed223494b92d3341b8d6f953f8c03b
digitalengineer07 <nikhil119124@gmail.com>
2026-08-06 15:56:51 +0530

    refactor: update database connection file path in test_db.php

--- test_db.php
+++ test_db.php
@@ -1,5 +1,5 @@
 <?php
-require 'includes/db.php';
+require 'db.php';
 $res = mysqli_query($conn, "SELECT e.* FROM electricity e JOIN users u ON e.user_id = u.id WHERE u.name LIKE '%Anurag%' ORDER BY e.id DESC LIMIT 1");
 print_r(mysqli_fetch_assoc($res));
 ?>
