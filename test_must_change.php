622b775ba0e5b216322a9dcd5d1e00a5eb73fc52
digitalengineer07 <nikhil119124@gmail.com>
2026-08-09 03:39:27 +0530

    refactor: update query to fetch all users and add completion message to test script

--- test_must_change.php
+++ test_must_change.php
@@ -1,7 +1,8 @@
 <?php
 require_once "db.php";
-$q = mysqli_query($conn, "SELECT id, username, name, must_change_password FROM users WHERE must_change_password = 1");
-if($q) {
-    while($row = mysqli_fetch_assoc($q)) { print_r($row); }
-} else { echo mysqli_error($conn); }
+$q = mysqli_query($conn, "SELECT id, username, name, must_change_password FROM users");
+while($row = mysqli_fetch_assoc($q)) {
+    print_r($row);
+}
+echo "done";
 ?>
