753e030a724a5981fca4642b82f21f6a74ceacdf
digitalengineer07 <nikhil119124@gmail.com>
2026-08-08 18:39:01 +0530

    refactor: update payments query and regenerate test output for user 5

--- test_totals.php
+++ test_totals.php
@@ -1,6 +1,6 @@
 <?php
 require 'db.php';
-$q = mysqli_query($conn, "SELECT id, bill_id, paid_amount, payment_date, transaction_id, sys_tx_id FROM payments WHERE user_id=5");
+$q = mysqli_query($conn, "SELECT id, bill_type, bill_id, sys_tx_id, paid_amount FROM payments WHERE user_id=5");
 while($r = mysqli_fetch_assoc($q)) {
     print_r($r);
 }
