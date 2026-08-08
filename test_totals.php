c0e17d7a7320d10dcf6de7f689f1de44408641eb
digitalengineer07 <nikhil119124@gmail.com>
2026-08-08 18:43:13 +0530

    chore: remove temporary test script and output file

--- test_totals.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'db.php';
-$q = mysqli_query($conn, "SELECT id, bill_type, bill_id, sys_tx_id, paid_amount FROM payments WHERE user_id=5");
-while($r = mysqli_fetch_assoc($q)) {
-    print_r($r);
-}
-?>
